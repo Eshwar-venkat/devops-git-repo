c4c4c3c204bef77b40db8041dc7d3bf6812b0f4a
Eshwar-venkat <riovenkatrio@gmail.com>
2026-02-17 00:34:26 +0530

    CICD test 1
    docker cicd deploy

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    # Run on port 5000, matching Dockerfile EXPOSE su
+    # Run on port 5000, matching Dockerfile EXPOSE
     app.run(host="0.0.0.0", port=5000)
